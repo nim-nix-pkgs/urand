c02675cd70362523f60114f1db56b2a34f492120
hugosenari <hugosenari@gmail.com>
2022-03-12 01:46:36 -0300

    chore: re index urand

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Simple method of obtaining secure random numbers from the OS'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."urand-master".url = "path:./master";
+  inputs."urand-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."urand-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
